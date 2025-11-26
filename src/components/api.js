64a489e52158fbd459f7b3aab19d4d82bbf3202b
Your Name <your-email@example.com>
2025-11-26 20:47:49 +0200

    edit on thing

--- src/components/api.js
+++ src/components/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api/", // your backend URL
+  // baseURL: "http://localhost:5000/api/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add token automatically
