88849bcd8049df0ed14dbffb75e1bbb7c9c8d435
Your Name <your-email@example.com>
2025-11-26 22:08:03 +0200

    edit on path of socket.io

--- src/components/CustomerTracking.jsx
+++ src/components/CustomerTracking.jsx
@@ -25,7 +25,7 @@ const homeIcon = new L.Icon({
     popupAnchor: [0, -36]
 });
 
-const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5000";
+const SOCKET_URL = import.meta.env.VITE_BACKEND_URL || "https://tawsila-mp8n.onrender.com";
 
 // function MapController({ driverLoc, customerLoc }) {
 //     const map = useMap();
